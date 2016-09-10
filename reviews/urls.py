d517664ca1946a384690c22a6a7d0d862aefca6f
Neeraj Gahlot <neeraj.gahlot@delhivery.com>
2016-09-10 09:43:48 +0530

    added correct sentiment counts

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^category', views.category, name='category')
+    url(r'^category', views.category, name='category'),
+    url(r'^sentiments/(?P<nature>.+$)', views.sentiments, name='sentiment')
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -5,7 +5,11 @@ from .models import Review
 import json
 
 def index(request):
-    return render(request, 'index.html', {})
+    total_count = Review.objects.count()
+    pos_count = Review.objects.filter(sentiment__iexact='positive').count()
+    neg_count = Review.objects.filter(sentiment__iexact='negative').count()
+    return render(request, 'index.html',
+        {'total_count': total_count, 'pos_count': pos_count, 'neg_count': neg_count})
 
 def category(request):
     results = Review.objects.values('source').\
@@ -14,3 +18,12 @@ def category(request):
     for result in results:
         data[result['source']] = result['dcount']
     return render(request, 'category.html', {'results': json.dumps(data)})
+
+def sentiments(request, nature):
+    filter_dict = {}
+    if nature == 'negative':
+        filter_dict['sentiment__iexact'] = 'negative'
+    elif nature == 'positive':
+        filter_dict['sentiment__iexact'] = 'positive'
+    reviews = Review.objects.filter(**filter_dict)
+    return render(request, 'sentiments.html', {'reviews': list(reviews)})
